61c02e79185df83cff7d0e1e3eeaf2670c685683
VyacheslavBakashov <bakashov@bk.ru>
2023-05-11 21:44:55 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ajs_hw4_2_matchers
+
+
+[![Build status](https://ci.appveyor.com/api/projects/status/ueo0y6osq8f4c1d7?svg=true)](https://ci.appveyor.com/project/VyacheslavBakashov/ajs-hw4-2-matchers)
---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,17 @@
+import sortCharacters from '../app';
+
+test('Сортировка по здоровью', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const received = sortCharacters(input);
+
+  expect(received).toEqual(expected);
+});
